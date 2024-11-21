da8a8d21f3e8fa4d381528acdc61f7615fccc33d
Geymark-sw <emmanuelpapio12@gmail.com>
2024-11-21 13:32:36 +0100

    prima applicazione react

---
+++ src/componenti/Link.jsx
@@ -0,0 +1,7 @@
+function Link(){
+    return(
+        <a href="https://substackcdn.com/image/fetch/w_848,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fa1a5ba1d-3fd1-4d67-88e4-6bc241e74ffa_1000x1000.jpeg">MA Ciao!!</a>
+    )
+}
+
+export default Link
---
+++ src/componenti/Navbar.jsx
@@ -0,0 +1,21 @@
+import './Navbar.css'
+import Link from './Link'
+
+function Navbar(){
+    return(
+
+        <ul>
+            <li><Link></Link></li>
+            <li><Link></Link></li>
+            <li><Link></Link></li>
+            <li><Link></Link></li>
+            <li><Link></Link></li>
+            <li><Link></Link></li>
+        </ul>
+
+    )
+}
+
+export default Navbar
+
+//https://substackcdn.com/image/fetch/w_848,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fa1a5ba1d-3fd1-4d67-88e4-6bc241e74ffa_1000x1000.jpeg
