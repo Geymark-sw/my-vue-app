33f76b3fd2af2de85c02b858b9c6ede2e6f124c2
Geymark-sw <emmanuelpapio12@gmail.com>
2024-11-21 13:52:17 +0100

    link

--- src/componenti/Link.jsx
+++ src/componenti/Link.jsx
@@ -1,6 +1,6 @@
 function Link(){
     return(
-        <a href="https://substackcdn.com/image/fetch/w_848,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fa1a5ba1d-3fd1-4d67-88e4-6bc241e74ffa_1000x1000.jpeg">MA Ciao!!</a>
+        <a href="https://www.kleisma.com/PublicData/Images/UserProfileImages/alessandro-sereni10001607.jpg?width=360&height=360&mode=crop">MA Ciao!!</a>
     )
 }
 
